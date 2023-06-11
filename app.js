ec730f9c1a69ff36c2a34ff893a703a07a0fe170
Jasperdelight <Jasperdelightt@gmail.com>
2023-06-10 21:53:23 -0600

    no video

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function scrollBottom() {
+  window.scrollTo(0, 1600);
+};
+
+
+// FIXED! NOTE TO SELF-DONT USE SCROLLTOP AS A FUNCTION NAME 
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+};
+
